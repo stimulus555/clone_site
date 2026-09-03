3378ddb9b583b231e191bc2b537573b10ca77d9d
stimulus555 <personaluseonly55555@gmail.com>
2026-09-03 23:49:50 +0530

    Insert international section after hero section

---
+++ section-order.js
@@ -0,0 +1,3 @@
+const hero = document.querySelector('.hero');
+const internationalSection = document.querySelector('.international');
+if (hero && internationalSection) hero.insertAdjacentElement('afterend', internationalSection);
