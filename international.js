ab21afb763b2e6d47e6960d385088f6675a47427
stimulus555 <personaluseonly55555@gmail.com>
2026-09-04 00:03:02 +0530

    Delete international.js

--- international.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('[data-fee-file]').forEach((button) => {
-  button.addEventListener('click', () => {
-    const region = button.dataset.feeFile === 'saarc' ? 'SAARC' : 'non-SAARC';
-    const toast = document.getElementById('toast');
-    toast.textContent = `The ${region} fee-structure file will open here once it is uploaded.`;
-    toast.classList.add('show');
-    setTimeout(() => toast.classList.remove('show'), 3600);
-  });
-});
