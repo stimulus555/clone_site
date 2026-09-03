14a79db29d4445fcbe1ce350e285ebae232c1572
stimulus555 <personaluseonly55555@gmail.com>
2026-09-04 00:03:26 +0530

    Delete section-order.js

--- section-order.js
+++
@@ -1,3 +0,0 @@
-const hero = document.querySelector('.hero');
-const internationalSection = document.querySelector('.international');
-if (hero && internationalSection) hero.insertAdjacentElement('afterend', internationalSection);
